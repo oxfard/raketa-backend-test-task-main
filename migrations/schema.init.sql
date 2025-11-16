070b078594d4ec95a67dd995281b4258a910debf
Oxfard <oxfard@mail.ru>
2025-11-16 18:48:50 +0700

    Обнавлена миграция. Категории вынесены в отдельную тбл.

--- migrations/schema.init.sql
+++ migrations/schema.init.sql
@@ -1,13 +1,22 @@
+create table if not exists categories
+(
+    id bigint unsigned auto_increment primary key,
+    name varchar(255) not null comment 'Наименование категории товара',
+    description text null comment 'Описание категории товара'
+)
+    comment 'Категории товаров';
+
 create table if not exists products
 (
-    id int auto_increment primary key,
-    uuid  varchar(255) not null comment 'UUID товара',
-    category  varchar(255) not null comment 'Категория товара',
+    id bigint unsigned auto_increment primary key,
+    uuid char(36) not null unique comment 'UUID товара',
+    category_id bigint unsigned not null comment 'ID категории товара',
     is_active tinyint default 1  not null comment 'Флаг активности',
-    name text default '' not null comment 'Тип услуги',
+    name varchar(255) not null comment 'Имя товара',
     description text null comment 'Описание товара',
-    thumbnail  varchar(255) null comment 'Ссылка на картинку',
-    price float not null comment 'Цена'
+    thumbnail varchar(255) null comment 'Ссылка на картинку',
+    price decimal(10, 2) not null comment 'Цена',
+    foreign key (category_id) references categories(id) on delete restrict
 )
     comment 'Товары';
 
